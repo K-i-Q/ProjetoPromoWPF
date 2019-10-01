a7797f9012ba08cb6ab55f22ef009e0402c1c2c9
Carlos Henrique <carlos.oli1505@gmail.com>
2019-09-30 21:57:49 -0300

    Adicionado exceção para tratamento de alteração de banco de dados com chave estrangeira ativa.

--- ProjetoPromoWPF/View/pgExcluirPerfil.xaml.cs
+++ ProjetoPromoWPF/View/pgExcluirPerfil.xaml.cs
@@ -2,6 +2,7 @@
 using ProjetoPromoWPF.Model;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,11 +24,37 @@ namespace ProjetoPromoWPF.View
     public partial class pgExcluirPerfil : Page
     {
         Empresa empresa;
+        frmLogin login;
         public pgExcluirPerfil(Empresa e)
         {
             InitializeComponent();
 
             empresa = EmpresaDAO.FindCompanyById(e.EmpresaId);
+            confirmacaoDaExclusaoDaEmpresa(empresa);
+        }
+
+        private void confirmacaoDaExclusaoDaEmpresa(Empresa empresa)
+        {
+            if (MessageBox.Show("Tem certeza de que deseja excluir sua conta? Esta operação não pode ser revertida.", "Exclusão de Conta", MessageBoxButton.YesNo,MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                login = new frmLogin();
+                try
+                {
+                    EmpresaDAO.RemoveCompany(empresa);
+                    MessageBox.Show("Sua conta foi excluída com sucesso, você será redirecionado para página de Login");
+                    login.Show();
+                    Window.GetWindow(this).Close();
+                }
+                catch (DbUpdateException)
+                {
+                    MessageBox.Show("Primeiro você precisa deletar todos os seus benefícios, planos e parcerias. Faça isso e tente novamente");
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("Operação Cancelada!");
+            }
         }
     }
 }
